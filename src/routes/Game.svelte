b001adc46a7cb944aed78dff357102cc30de2b85
MeirJC <meir84@gmail.com>
2024-06-14 17:55:31 +0300

    initial layout

---
+++ src/routes/Game.svelte
@@ -0,0 +1,31 @@
+<div class="game">
+  <div class="info"></div>
+  <div class="grid-container">
+
+  </div>
+  <div class="info"></div>
+</div>
+
+<style>
+  .game {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100dvh;
+  }
+
+  .grid-container {
+    display: grid;
+    width: 80vmin;
+    height: 80vmin;
+    background-color: deeppink;
+  }
+
+  .info {
+    font-size: 24px;
+    width: 80vmin;
+    height: 10vmin;
+    background-color: darkviolet;
+  }
+</style>
